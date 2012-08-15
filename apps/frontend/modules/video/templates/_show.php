c9cb7e1ac437eee3481b550580387c1f1653136e
saynt2day <saynt2day@gmail.com>
2012-08-15 13:55:39 -0700

    #469 Remove hint for videos: click "CC" for subtitles

--- apps/frontend/modules/video/templates/_show.php
+++ apps/frontend/modules/video/templates/_show.php
@@ -35,7 +35,7 @@
 		<?php if (empty($short)): ?>
 			<div class="video_info">
 				<p class="date right_text p1_no_both">
-                    <span class="left"><?php echo __('Click "CC" in the player to show captions') ?></span>
+                    <?php /*<span class="left"><?php echo __('Click "CC" in the player to show captions') ?></span>*/ ?>
 					<strong><?php echo format_date( $video->getCreatedAt(), 'd MMMM yyyy' ); ?></strong>
 				</p>
 				<?php 
