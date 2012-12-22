6586936e6f48d29874be1c950f11ac1d61dd0ba2
saynt2day <saynt2day@gmail.com>
2012-12-23 01:18:14 +0400

    #653 Style quotes author

--- apps/frontend/modules/text/templates/jsSuccess.php
+++ apps/frontend/modules/text/templates/jsSuccess.php
@@ -1167,7 +1167,7 @@ function runCarousel()
             } else {
                 quote_url = '#';
             }
-            carousel_html = carousel_html + '<a href="'+quote_url+'" title="<?php echo __("Read more") ?>"><p>'+quote_text+'<br/><span class="right">― <?php echo __("Maha Sambodhi Dharma Sangha") ?></span></p></a>';
+            carousel_html = carousel_html + '<a href="'+quote_url+'" title="<?php echo __("Read more") ?>"><p>'+quote_text+'<br/><span class="right"><?php echo __("Maha Sambodhi Dharma Sangha") ?></span></p></a>';
             carousel_html = carousel_html + '</div></div>';
         }
 
