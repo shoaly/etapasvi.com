854d11fb5c32332a072e1585b83a2f997a074c30
saynt2day <saynt2day@gmail.com>
2013-01-11 20:40:21 +0400

    #735 Category HTML error

--- apps/frontend/modules/itemcategory/templates/_show.php
+++ apps/frontend/modules/itemcategory/templates/_show.php
@@ -5,5 +5,5 @@
         <?php foreach($itemcategory_tree as $element): ?>
             <?php include_partial('itemcategory/tree_element', array('module_action'=>$module_action, 'element'=>$element, 'item_type_list'=>$item_type_list) ); ?>
         <?php endforeach ?>
-    <select>
+    </select>
 </div>
