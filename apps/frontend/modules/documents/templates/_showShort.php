36ee396666e912dbc3efee548f5cbcc4eab139d0
saynt2day <saynt2day@gmail.com>
2013-01-16 15:51:15 +0400

    #734 Documents design is broken in IE 7

--- apps/frontend/modules/documents/templates/_showShort.php
+++ apps/frontend/modules/documents/templates/_showShort.php
@@ -8,7 +8,7 @@
     <?php endif ?>
     <tr <?php if (!$last): ?>class="middle"<?php endif ?>>
         <td class="main_col"><a href="<?php echo $documents->getUrl(); ?>"><?php echo $title; ?></a></td>
-        <td><span class="files <?php echo $documents->getType(); ?>"><?php echo $documents->getType(); ?></span></td>
+        <td><div class="files <?php echo $documents->getType(); ?>"><?php echo $documents->getType(); ?></div></td>
         <td><?php echo $documents->getSizePrepared(); ?></td>
         <?php /*<td><?php echo format_datetime( $documents->getCreatedAt(), 'd MMMM yyyy'); ?></td>
         <td><?php echo format_datetime( $documents->getUpdatedAt(), 'd MMMM yyyy'); ?></td>
