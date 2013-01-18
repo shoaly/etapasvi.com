44cb6fc6752c1514e6ef37a6f66ac0d27d4b5315
saynt2day <saynt2day@gmail.com>
2013-01-18 22:44:46 +0400

    #786 New like button script

--- apps/frontend/templates/_like.php
+++ apps/frontend/templates/_like.php
@@ -1,13 +1,9 @@
 <span class="likebtn-wrapper" data-identifier="<?php if ($identifier):?><?php echo $identifier; ?><?php endif ?>" data-dislike_enabled="0" data-i18n_like="<?php echo __('Like') ?>" data-i18n_dislike="<?php echo __('Dislike') ?>" data-i18n_like_tooltip="<?php echo __('I like this') ?>" data-i18n_dislike_tooltip="<?php echo __('I dislike this') ?>" data-i18n_unlike_tooltip="<?php echo __('Unlike') ?>" data-i18n_undislike_tooltip="<?php echo __('Undislike') ?>" data-i18n_share_text="<?php echo __('Would you like to share?') ?>" data-i18n_share_close="<?php echo __('Close') ?>"></span>
+<?php if ($init): ?>
 <script type="text/javascript">
-    <?php if ($init): ?>
-        if (typeof(window.LikeBtn) != "undefined" && window.LikeBtn) {
-            window.LikeBtn = null;
-        }
-    <?php endif ?>
-    (function(d) {
-        var likebtn_s = d.createElement('script'); likebtn_s.type = 'text/javascript'; likebtn_s.async = true;
-        likebtn_s.src = 'http://www.likebtn.com/widget/';
-        (d.getElementsByTagName('head')[0] || d.getElementsByTagName('body')[0]).appendChild(likebtn_s);    
-    })(document);
+    if (typeof(window.LikeBtn) != "undefined" && window.LikeBtn) {
+        window.LikeBtn = null;
+    }
 </script>
+<?php endif ?>
+<script type="text/javascript" src="http://www.likebtn.com/widget/" async="async"></script>
