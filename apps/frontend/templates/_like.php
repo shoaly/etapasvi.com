fc380dd85e257e50740b403ea7527127275bf415
saynt2day <saynt2day@gmail.com>
2013-01-19 09:12:08 +0400

    #789 Update like button code

--- apps/frontend/templates/_like.php
+++ apps/frontend/templates/_like.php
@@ -1,4 +1,4 @@
-<span class="likebtn-wrapper" data-identifier="<?php if ($identifier):?><?php echo $identifier; ?><?php endif ?>" data-dislike_enabled="0" data-i18n_like="<?php echo __('Like') ?>" data-i18n_dislike="<?php echo __('Dislike') ?>" data-i18n_like_tooltip="<?php echo __('I like this') ?>" data-i18n_dislike_tooltip="<?php echo __('I dislike this') ?>" data-i18n_unlike_tooltip="<?php echo __('Unlike') ?>" data-i18n_undislike_tooltip="<?php echo __('Undislike') ?>" data-i18n_share_text="<?php echo __('Would you like to share?') ?>" data-i18n_share_close="<?php echo __('Close') ?>"></span>
+<span class="likebtn-wrapper" data-identifier="<?php if ($identifier):?><?php echo $identifier; ?><?php endif ?>" data-show_like_label="0" data-dislike_enabled="0" data-color_scheme="transparent" data-i18n_like_tooltip="<?php echo __('I like this') ?>" data-i18n_dislike_tooltip="<?php echo __('I dislike this') ?>" data-i18n_unlike_tooltip="<?php echo __('Unlike') ?>" data-i18n_undislike_tooltip="<?php echo __('Undislike') ?>" data-i18n_share_text="<?php echo __('Would you like to share?') ?>" data-i18n_share_close="<?php echo __('Close') ?>"></span>
 <?php if ($init): ?>
 <script type="text/javascript">
     if (typeof(window.LikeBtn) != "undefined" && window.LikeBtn) {
@@ -6,4 +6,4 @@
     }
 </script>
 <?php endif ?>
-<script type="text/javascript" src="http://www.likebtn.com/widget/" async="async"></script>
+<script type="text/javascript" src="http://www.likebtn.com/js/widget.js" async="async"></script>
