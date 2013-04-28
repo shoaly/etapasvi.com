c804a671b7c21f4ccad3466a231d518bacacc1b2
saynt2day <saynt2day@gmail.com>
2013-04-28 22:38:50 +0400

    #1111 Download teachings as document

--- apps/frontend/modules/documents/templates/_showTeachings.php
+++ apps/frontend/modules/documents/templates/_showTeachings.php
@@ -1,3 +1,3 @@
 <?php if ($document): ?>
-    <?php include_partial('documents/showShort', array('documents'=>$document, 'short'=>true)); ?>
+    <?php include_partial('documents/showShort', array('documents'=>$document, 'short'=>true, 'last'=>true)); ?>
 <?php endif; ?>
