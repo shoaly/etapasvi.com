8e660c7933bb21a818d6159073ea0751b473598a
saynt2day <saynt2day@gmail.com>
2013-05-04 19:04:59 +0400

    #1133 Hide FAQ

--- apps/frontend/templates/_menu.php
+++ apps/frontend/templates/_menu.php
@@ -1,10 +1,10 @@
 <ul>
 		<li id="mi_main"><a href="<?php echo url_for('@main', true); ?>" title="<?php echo __('Home') ?>" ><?php echo __('Home') ?></a></li>
 	<?php /*</ul>
-	<h3><?php echo __('Dharma Sangha') ?></h3> 
+	<h3><?php echo __('Dharma Sangha') ?></h3>
 	<ul>*/ ?>
 		<?php /*<li><a href="<?php echo url_for('@intro', true); ?>" title="<?php echo __('Buddha Boy Nepal') ?>" id="mi_intro"><?php echo __('Intro') ?></a></li> */ ?>
-        <li id="mi_news"><a href="<?php echo url_for('@news_index', true); ?>" title="<?php echo __('News') ?>"><?php echo __('News') ?></a></li>        
+        <li id="mi_news"><a href="<?php echo url_for('@news_index', true); ?>" title="<?php echo __('News') ?>"><?php echo __('News') ?></a></li>
         <li id="mi_teachings"><i>»</i> <a href="<?php echo url_for('news/index?itemcategory=teachings', true); ?>" title="<?php echo __('Teachings') ?>"><?php echo __('Teachings') ?></a></li>
         <li id="mi_precepts"><i>» »</i> <a href="<?php echo url_for('news/index?itemcategory=precepts', true); ?>" title="<?php echo __('Precepts') ?>"><?php echo __('Precepts') ?></a></li>
         <?php /*<li id="mi_stories"><i>»</i> <a href="<?php echo url_for('@news/index?itemcategory=teachings', true); ?>" title="<?php echo __('Stories') ?>"><?php echo __('Stories') ?></a></li>*/ ?>
@@ -14,13 +14,13 @@
 		<li id="mi_dharmic_forest"><i>» »</i> <a href="http://www.savenepaltapasviforest.info" title="<?php echo __('Dharmic Forest') ?>" target="_blank"><?php echo __('Dharmic Forest') ?></a></li>
 		<li id="mi_photo"><a href="<?php echo url_for('@photoalbum_index', true); ?>" title="<?php echo __('Photo') ?>"><?php echo __('Photo') ?></a></li>
 		<li id="mi_video"><a href="<?php echo url_for('@video_index', true); ?>" title="<?php echo __('Video') ?>"><?php echo __('Video') ?></a></li>
-		<li id="mi_audio"><a href="<?php echo url_for('@audio_index', true); ?>" title="<?php echo __('Audio') ?>"><?php echo __('Audio') ?></a></li>               
-		<li id="mi_documents"><a href="<?php echo url_for('@documents_index', true); ?>" title="<?php echo __('Documents') ?>"><?php echo __('Documents') ?></a></li>               
+		<li id="mi_audio"><a href="<?php echo url_for('@audio_index', true); ?>" title="<?php echo __('Audio') ?>"><?php echo __('Audio') ?></a></li>
+		<li id="mi_documents"><a href="<?php echo url_for('@documents_index', true); ?>" title="<?php echo __('Documents') ?>"><?php echo __('Documents') ?></a></li>
         <li id="mi_feed"><a href="<?php echo url_for('@feed', true); ?>" title="<?php echo __('Feed') ?>"><?php echo __('Feed') ?></a></li>
-        <li id="mi_bio"><a href="<?php echo url_for('@biography', true); ?>" title="<?php echo __('Dharma Sangha') ?> – <?php echo __('Biography') ?>"><?php echo __('Biography') ?></a></li>		
+        <li id="mi_bio"><a href="<?php echo url_for('@biography', true); ?>" title="<?php echo __('Dharma Sangha') ?> – <?php echo __('Biography') ?>"><?php echo __('Biography') ?></a></li>
 		<li id="mi_blessing"><a href="<?php echo url_for('@blessing', true); ?>" title="<?php echo __('Blessing') ?>"><?php echo __('Blessing') ?></a></li>
-        <li id="mi_faq"><a href="<?php echo url_for('@faq', true); ?>" title="<?php echo __('FAQ') ?>"><?php echo __('FAQ') ?></a></li>
-        <li id="mi_encyclopedia"><a href="<?php echo url_for('@encyclopedia', true); ?>" title="<?php echo __('Encyclopedia') ?>"><?php echo __('Encyclopedia') ?></a></li>        
+        <?php /*<li id="mi_faq"><a href="<?php echo url_for('@faq', true); ?>" title="<?php echo __('FAQ') ?>"><?php echo __('FAQ') ?></a></li>*/ ?>
+        <li id="mi_encyclopedia"><a href="<?php echo url_for('@encyclopedia', true); ?>" title="<?php echo __('Encyclopedia') ?>"><?php echo __('Encyclopedia') ?></a></li>
 <?php /*
 		<li id="mi_religious_forest"><a href="<?php echo url_for('@religious_forest', true); ?>" title="<?php echo __('Religious Forest') ?>"><?php echo __('Religious Forest') ?></a></li>
 */
@@ -31,10 +31,10 @@
 
 	<?php /*</ul>
 
-	<h3><?php echo __('Member') ?></h3> 
+	<h3><?php echo __('Member') ?></h3>
 	<ul>
 		<?php if (!UserPeer::authIsLoggedIn() ): ?>
-			<li><a href="<?php echo url_for('@user_signup', true); ?>" title="<?php echo __('Sign up') ?>" id="mi_signup"><?php echo __('Sign up') ?></a></li>		
+			<li><a href="<?php echo url_for('@user_signup', true); ?>" title="<?php echo __('Sign up') ?>" id="mi_signup"><?php echo __('Sign up') ?></a></li>
 			<li><a href="<?php echo url_for('@user_login', true); ?>" title="<?php echo __('Login') ?>" id="mi_login"><?php echo __('Login') ?></a></li>
         <?php else: ?>
 			<li><a href="<?php echo url_for('@user_profile', true); ?>" title="<?php echo __('Edit profile') ?>" id="mi_profile"><?php echo __('Profile') ?></a></li>
@@ -64,8 +64,8 @@
 */?>
 	<?php /*<h3><?php echo __('Other') ?></h3>
 	<ul>*/?>
-		<?php /*<li><a href="<?php echo url_for('@user_member', true); ?>" title="<?php echo __('Member') ?>" id="mi_member"><?php echo __('Member') ?></a></li> */?>        
-		<li id="mi_social_tools"><a href="<?php echo url_for('@social_tools', true); ?>" title="<?php echo __('Social Tools') ?>"><?php echo __('Social Tools') ?></a></li>        
+		<?php /*<li><a href="<?php echo url_for('@user_member', true); ?>" title="<?php echo __('Member') ?>" id="mi_member"><?php echo __('Member') ?></a></li> */?>
+		<li id="mi_social_tools"><a href="<?php echo url_for('@social_tools', true); ?>" title="<?php echo __('Social Tools') ?>"><?php echo __('Social Tools') ?></a></li>
 		<?php /*<li id="mi_chat"><i>»</i> <a href="<?php echo url_for('@chat', true); ?>" title="<?php echo __('Chat') ?>"><?php echo __('Chat') ?></a></li>*/ ?>
 		<?php /*<li id="mi_forum"><a href="http://forum.etapasvi.com" title="<?php echo __('Forum') ?>"><?php echo __('Forum') ?></a></li>*/ ?>
 		<li id="mi_search"><a href="<?php echo url_for('@search', true); ?>" title="<?php echo __('Search') ?>"><?php echo __('Search') ?></a></li>
@@ -78,7 +78,7 @@
             </form>
         </li>
 	</ul>
-<?php /* 
+<?php /*
 	<h3><?php echo __('Idea of the Week') ?></h3>
 	<ul>
 		<li><a href="<?php echo url_for('@idea_ideaoftheweek', true); ?>" title="<?php echo __('What is this') ?>" id="mi_ideaoftheweek"><?php echo __('What is this') ?></a></li>
