2513926f3bdb07a18829baaeef0cabb27d1fb385
saynt2day <saynt2day@gmail.com>
2013-06-02 19:37:56 +0400

    #1252 Link rel

--- apps/frontend/modules/social_tools/templates/_show.php
+++ apps/frontend/modules/social_tools/templates/_show.php
@@ -9,33 +9,33 @@
     <? */ ?>
     <?php /* <a href="<?php echo url_for('@chat', true); ?>" target="_blank" title="<?php echo __('Chat') ?>" class="social_zoho"><span><?php echo __('Chat') ?></span></a> */ ?>
     <?php /*<a href="http://kiwi6.com/users/show/etapasvi" target="_blank" title="<?php echo __('Kiwi6') ?>" class="social_kiwi6"><span><?php echo __('Kiwi6') ?></span></a>*/ ?>
-    <a href="http://twitter.com/<?php echo UserPeer::getCultureTwitter(); ?>" target="_blank" title="<?php echo __('Twitter') ?>" class="social_twitter"><span><?php echo __('Twitter') ?></span></a>
+    <a href="http://twitter.com/<?php echo UserPeer::getCultureTwitter(); ?>" target="_blank" title="<?php echo __('Twitter') ?>" class="social_twitter" rel="nofollow"><span><?php echo __('Twitter') ?></span></a>
 
     <?php if ($sf_user->getCulture() == 'ru'): ?>
          <?php /* <a href="http://vaikuntha.ru/blog/dharmas/" target="_blank" title="<?php echo __('Вайкунтха') ?>" class="social_vaikuntha"><span><?php echo __('Вайкунтха') ?></span></a> */ ?>
-         <a href="http://vk.com/etapasvi" target="_blank" title="<?php echo __('ВКонтакте') ?>" class="social_vkontakte"><span><?php echo __('ВКонтакте') ?></span></a><br/>
+         <a href="http://vk.com/etapasvi" target="_blank" title="<?php echo __('ВКонтакте') ?>" class="social_vkontakte" rel="nofollow"><span><?php echo __('ВКонтакте') ?></span></a><br/>
     <?php /* <?php else: ?>
         <a href="http://etapasvi.livejournal.com/" target="_blank" title="<?php echo __('Live Journal') ?>" class="social_livejournal"><span><?php echo __('Live Journal') ?></span></a> */ ?>
     <?php endif ?>
 
-    <a href="http://www.facebook.com/<?php echo UserPeer::getCultureFbGroup(); ?>/" target="_blank" title="<?php echo __('Facebook') ?>" class="social_facebook"><span><?php echo __('Facebook') ?></span></a>
+    <a href="http://www.facebook.com/<?php echo UserPeer::getCultureFbGroup(); ?>/" target="_blank" title="<?php echo __('Facebook') ?>" class="social_facebook" rel="nofollow"><span><?php echo __('Facebook') ?></span></a>
     <?php if ($sf_user->getCulture() != 'ru'): ?><br/><?php endif ?>
     <?php if ($sf_user->getCulture() == 'ja'): ?>
-        <a href="http://groups.google.com/group/dharmasangha-jp" target="_blank" title="<?php echo __('Google') ?>" class="social_google"><span><?php echo __('Google') ?></span></a>
+        <a href="http://groups.google.com/group/dharmasangha-jp" target="_blank" title="<?php echo __('Google') ?>" class="social_google" rel="nofollow"><span><?php echo __('Google') ?></span></a>
     <?php elseif ($sf_user->getCulture() != 'ru'): ?>
-        <a href="https://groups.google.com/forum/?fromgroups=#!forum/maha-sambodhi-dharma-sangha-" target="_blank" title="<?php echo __('Google') ?>" class="social_google"><span><?php echo __('Google') ?></span></a>
+        <a href="https://groups.google.com/forum/?fromgroups=#!forum/maha-sambodhi-dharma-sangha-" target="_blank" title="<?php echo __('Google') ?>" class="social_google" rel="nofollow"><span><?php echo __('Google') ?></span></a>
     <? endif ?>
-    <a href="http://whos.amung.us/stats/ppc9yoe3440f/" target="_blank" title="<?php echo __('Visitor Map') ?>" class="social_visitor_map"><span><?php echo __('Visitor Map') ?></span></a>
-    <a href="http://www.youtube.com/user/etapasvi" target="_blank" title="<?php echo __('YouTube') ?>" class="social_youtube"><span><?php echo __('YouTube') ?></span></a>
+    <a href="http://whos.amung.us/stats/ppc9yoe3440f/" target="_blank" title="<?php echo __('Visitor Map') ?>" class="social_visitor_map" rel="nofollow"><span><?php echo __('Visitor Map') ?></span></a>
+    <a href="http://www.youtube.com/user/etapasvi" target="_blank" title="<?php echo __('YouTube') ?>" class="social_youtube" rel="nofollow"><span><?php echo __('YouTube') ?></span></a>
 
     <?php if ($sf_user->getCulture() == 'ru'): ?>
-        <a href="http://odnoklassniki.ru/group/52853546877166" target="_blank" title="<?php echo __('Одноклассники') ?>" class="social_odnoklassniki"><span><?php echo __('Одноклассники') ?></span></a>
+        <a href="http://odnoklassniki.ru/group/52853546877166" target="_blank" title="<?php echo __('Одноклассники') ?>" class="social_odnoklassniki" rel="nofollow"><span><?php echo __('Одноклассники') ?></span></a>
     <? else: ?>
-        <a href="https://picasaweb.google.com/105320929368395530858" target="_blank" title="<?php echo __('Picasa') ?>" class="social_picasa"><span><?php echo __('Picasa') ?></span></a>
+        <a href="https://picasaweb.google.com/105320929368395530858" target="_blank" title="<?php echo __('Picasa') ?>" class="social_picasa" rel="nofollow"><span><?php echo __('Picasa') ?></span></a>
     <? endif ?>
     <br/>
-    <a href="https://github.com/etapasvi/etapasvi.com" target="_blank" title="<?php echo __('GitHub') ?>" class="social_github"><span><?php echo __('GitHub') ?></span></a>
-    <a href="http://feeds.feedburner.com/<?php echo $sf_user->getCulture(); ?>/etapasvi" target="_blank" title="<?php echo __('RSS') ?>" class="social_rss"><span><?php echo __('RSS') ?></span></a>
+    <a href="https://github.com/etapasvi/etapasvi.com" target="_blank" title="<?php echo __('GitHub') ?>" class="social_github" rel="nofollow"><span><?php echo __('GitHub') ?></span></a>
+    <a href="http://feeds.feedburner.com/<?php echo $sf_user->getCulture(); ?>/etapasvi" target="_blank" title="<?php echo __('RSS') ?>" class="social_rss" rel="nofollow"><span><?php echo __('RSS') ?></span></a>
     <?php if ($sf_user->getCulture() == 'ru'): ?>
-        <a href="http://my.mail.ru/community/etapasvi/" target="_blank" title="<?php echo __('Мой Мир@Mail.Ru') ?>" class="social_moimir"><span><?php echo __('Мой Мир@Mail.Ru') ?></span></a>
+        <a href="http://my.mail.ru/community/etapasvi/" target="_blank" title="<?php echo __('Мой Мир@Mail.Ru') ?>" class="social_moimir" rel="nofollow"><span><?php echo __('Мой Мир@Mail.Ru') ?></span></a>
     <? endif ?>
--- apps/frontend/templates/_toolbar.php
+++ apps/frontend/templates/_toolbar.php
@@ -5,7 +5,7 @@
     <a href="javascript:switchOfferTr('<?php echo url_for('offer_translation'); ?>', '<?php echo __('Error occured while loading form. Please, try again.') ?>')" id="offer_tr_trigger" class="page_tools_trigger" title="<?php echo __('Translate') ?>"><?php /*<i class="pt_btn pt_btn_offer_tr"></i>*/ ?><?php echo __('Translate') ?></a>
     <a href="javascript:switchEmbed()" id="embed_trigger" class="page_tools_trigger" title="<?php echo __('embed') ?>"><?php /*<i class="pt_btn pt_btn_embed"></i>*/ ?><?php echo __('Embed') ?></a>
     <a href="<?php echo $cur_url; ?>#print_version" target="_blank" id="print_version_trigger" class="page_tools_trigger" title="<?php echo __('print version') ?>"><?php /*<i class="pt_btn pt_btn_print_version"></i>*/?><?php echo __('Print version') ?></a>
-    <a href="http://www.web2pdfconvert.com/engine.aspx?cURL=<?php echo $cur_url; ?>%23print_version" target="_blank" class="page_tools_trigger" title="<?php echo __('PDF') ?>"><?php /*<i class="pt_btn pt_btn_pdf"></i>*/ ?><?php echo __('PDF') ?></a>
+    <a href="http://www.web2pdfconvert.com/engine.aspx?cURL=<?php echo $cur_url; ?>%23print_version" target="_blank" class="page_tools_trigger" title="<?php echo __('PDF') ?>" rel="nofollow"><?php /*<i class="pt_btn pt_btn_pdf"></i>*/ ?><?php echo __('PDF') ?></a>
 
     <?php /* должно быть между кнопками и блоками, т.к. в виде слота кнопку Revision history подключить не удалось */ ?>
     <?php include_component( 'revisionhistory', 'show' ); ?> 
