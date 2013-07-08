82b6b9124ee0ebe6fc4bc951a5ae0f37ea3f4206
saynt2day <saynt2day@gmail.com>
2013-07-08 15:40:18 +0400

    #1398 Redirects

--- apps/frontend/modules/contactus/actions/actions.class.php
+++ apps/frontend/modules/contactus/actions/actions.class.php
@@ -10,23 +10,32 @@
  */
 class contactusActions extends sfActions
 {
-	
+  /**
+   * Redirect to Sangha page
+   *
+   * @param sfWebRequest $request
+   */
+  public function executeRedirect(sfWebRequest $request)
+  {
+    $this->redirect($this->generateUrl('sangha'));
+  }
+
   public function executeShow(sfWebRequest $request)
   {
     //$this->chapter_list = TextPeer::getGoogleDocAsArray( 'https://spreadsheets.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0ApLTjOcBiwykdHBZbXpYTUJRWU1pdEdrZi1OTU9jb2c&single=true&gid=0&output=html' );
-      
+
     // get contacts from DB
     $c = new Criteria();
     $c->add(ContactusPeer::SHOW, 1);
     $c->addAscendingOrderByColumn( ContactusPeer::ORDER );
-      
+
     $contactus_db_list = ContactusPeer::doSelect($c);
-      
+
     $cultures_data  = UserPeer::getCulturesData();
     $contactus_list = array();
-      
+
     $cur_culture = sfContext::getInstance()->getUser()->getCulture();
-      
+
     foreach ($cultures_data as $culture => $culture_data) {
 
       $list = array();
@@ -52,7 +61,7 @@ class contactusActions extends sfActions
           'link'        => ContactusPeer::getTwitterLink(UserPeer::getCultureTwitter($culture))
         );
       }
-      
+
       foreach($contactus_db_list as $contactus_db) {
         if ($contactus_db->getCulture() != $culture) {
           continue;
@@ -70,9 +79,9 @@ class contactusActions extends sfActions
         'list'         => $list,
       );
     }
-     
+
     $this->contactus_list = array();
-    
+
     // show current language first
     foreach ($contactus_list as $i=>$contactus_item) {
       if ($contactus_item['culture'] == $cur_culture) {
@@ -81,8 +90,8 @@ class contactusActions extends sfActions
       	break;
       }
     }
-	
+
     $this->contactus_list = $contactus_list;
   }
-  
+
 }
