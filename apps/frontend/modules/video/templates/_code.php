3a5ae59fc284328b3bdae1434f7c5fff3d3bcd2c
saynt2day <saynt2day@gmail.com>
2013-07-11 17:37:57 +0400

    #1415 Wide screen video

--- apps/frontend/modules/video/templates/_code.php
+++ apps/frontend/modules/video/templates/_code.php
@@ -1,6 +1,6 @@
 <?php if (!empty($code)): ?>
     <?php if (strlen($code) <20): ?>
-    <iframe height="382" frameborder="0" width="<?php if (!empty($wide) && $wide): ?>566<?php else: ?>520<?php endif ?>" allowfullscreen="" src="http://www.youtube.com/embed/<?php echo $code; ?>?rel=0<?php if ($autoplay): ?>&autoplay=1<?php endif ?>&cc_load_policy=1&amp;hl=<?php echo UserPeer::getCultureMsdn(); ?>&wmode=opaque&cc_lang_pref=<?php echo UserPeer::getCultureMsdn(); ?>"></iframe>
+    <iframe height="382" frameborder="0" width="<?php if (!empty($wide) && $wide): ?>566<?php else: ?>564<?php endif ?>" allowfullscreen="" src="http://www.youtube.com/embed/<?php echo $code; ?>?rel=0<?php if ($autoplay): ?>&autoplay=1<?php endif ?>&cc_load_policy=1&amp;hl=<?php echo UserPeer::getCultureMsdn(); ?>&wmode=opaque&cc_lang_pref=<?php echo UserPeer::getCultureMsdn(); ?>"></iframe>
     <?php else: ?>
         <?php echo html_entity_decode($code); ?>
     <?php endif ?>
