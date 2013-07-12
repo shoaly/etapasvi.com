bc1e2fb257028db51140cef25d79522e3bade74d
saynt2day <saynt2day@gmail.com>
2013-07-12 16:48:24 +0400

    #1419 Photo list border

--- apps/frontend/modules/photo/templates/albumSuccess.php
+++ apps/frontend/modules/photo/templates/albumSuccess.php
@@ -18,33 +18,35 @@ $author = $photoalbum->getAllAuthors($sf_user->getCulture(), $photo_list);
 <p class="bread_crumbs">
 	<a href="<?php echo url_for('@main'); ?>"><?php echo __('Home') ?></a> » <a href="<?php echo url_for('photoalbum_index'); ?>"><?php echo __('Photo Albums') ?></a>
 </p>
-<div class="box photoalbum_container">
-<h1 class="title"><?php echo $photoalbum->getTitle($sf_user->getCulture(), true);?></h1>
-<p class="light center_text small">
-    <strong><?php echo __('Date') ?>:</strong> <?php echo format_datetime( $photoalbum->getCreatedAt(), 'd MMMM yyyy'); ?> |
-    <strong><?php echo __('Photo') ?>:</strong> <?php echo $photoalbum->countPhotos(); ?>
-    <?php if ($author): ?>
-        | <strong><?php echo __('Author') ?>:</strong> <?php echo $author ?>
+<div class="photoalbum_wrapper">
+    <div class="box photoalbum_container">
+    <h1 class="title"><?php echo $photoalbum->getTitle($sf_user->getCulture(), true);?></h1>
+    <p class="light center_text small">
+        <strong><?php echo __('Date') ?>:</strong> <?php echo format_datetime( $photoalbum->getCreatedAt(), 'd MMMM yyyy'); ?> |
+        <strong><?php echo __('Photo') ?>:</strong> <?php echo $photoalbum->countPhotos(); ?>
+        <?php if ($author): ?>
+            | <strong><?php echo __('Author') ?>:</strong> <?php echo $author ?>
+        <?php endif ?>
+    </p>
+    <?php include_component('itemcategory', 'showitemcategories', array(
+                            'item_type'     => ItemtypesPeer::ITEM_TYPE_PHOTOALBUM,
+                            'item_id'       => $photoalbum->getId(),
+                            'module_action' => 'photoalbums/index'));
+    ?>
+    <?php $body = $photoalbum->getBody($sf_user->getCulture(), true); ?>
+    <?php if($body): ?>
+        <p><?php echo $body;?></p>
     <?php endif ?>
-</p>
-<?php include_component('itemcategory', 'showitemcategories', array(
-                        'item_type'     => ItemtypesPeer::ITEM_TYPE_PHOTOALBUM,
-                        'item_id'       => $photoalbum->getId(),
-                        'module_action' => 'photoalbums/index'));
-?>
-<?php $body = $photoalbum->getBody($sf_user->getCulture(), true); ?>
-<?php if($body): ?>
-    <p><?php echo $body;?></p>
-<?php endif ?>
-</div>
+    </div>
 
-<?php if (count($photo_list )): ?>
-	<?php include_partial('photo/list', array('photo_list'=>$photo_list)); ?>
-<?php else: ?>
-	<p class="center error_list p2">
-		<?php echo __('No Photos') ?>
-	</p>
-<?php endif ?>
+    <?php if (count($photo_list )): ?>
+        <?php include_partial('photo/list', array('photo_list'=>$photo_list)); ?>
+    <?php else: ?>
+        <p class="center error_list p2">
+            <?php echo __('No Photos') ?>
+        </p>
+    <?php endif ?>
+</div>
 
 <?php /*echo $navigation_html;*/ ?>
 
